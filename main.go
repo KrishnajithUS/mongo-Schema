9e6680cbf71756d2c2c6dfbde4915d43c980ad97
KrishnajithUS <krishnajithus673@gmail.com>
2024-06-12 14:57:19 +0530

    [ monitor implemented ]

--- main.go
+++ main.go
@@ -2,18 +2,92 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
-	"time"
 	"main/queries"
+	"os"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/event"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var insertCount int = 0
+var eventArray []*event.ServerDescriptionChangedEvent
+
+var writeServers = make(map[string]int)
+var readServers = make(map[string]int)
+
+func logEvents() {
+	log.Println("Total Insertions:", insertCount*10)
+}
+
+
+var cmdMonitor *event.CommandMonitor = &event.CommandMonitor{
+	Started: func(_ context.Context, evt *event.CommandStartedEvent) {
+		if evt.CommandName == "insert" {
+			writeServers[evt.ConnectionID]++
+		}
+		if evt.CommandName == "find" {
+			readServers[evt.ConnectionID]++
+		}
+	},
+	Succeeded: func(_ context.Context, evt *event.CommandSucceededEvent) {
+		if evt.CommandName == "insert" {
+			insertCount++
+		}
+	},
+	Failed: func(_ context.Context, evt *event.CommandFailedEvent) {
+	},
+}
+
+var srvMonitor *event.ServerMonitor = &event.ServerMonitor{
+	ServerDescriptionChanged: func(e *event.ServerDescriptionChangedEvent) {
+		eventArray = append(eventArray, e)
+	},
+}
+func getReplicaSetStatus(client *mongo.Client, ctx context.Context) (primary string, secondaries []string, err error) {
+	var result bson.M
+	err = client.Database("admin").RunCommand(ctx, bson.D{{Key: "replSetGetStatus", Value: 1}}).Decode(&result)
+	if err != nil {
+		return "", nil, err
+	}
+
+	members := result["members"].(bson.A)
+	for _, member := range members {
+		memberMap := member.(bson.M)
+		stateStr := memberMap["stateStr"].(string)
+		name := memberMap["name"].(string)
+		if stateStr == "PRIMARY" {
+			primary = name
+		} else if stateStr == "SECONDARY" {
+			secondaries = append(secondaries, name)
+		}
+	}
+	return primary, secondaries, nil
+}
+
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017")) // connectionstring
+	
+	lst :=[]int{5, 50, 500, 5000}
+	if len(os.Args) < 1 {
+		panic("Usage: program [readPreference]")
+	}
+
+	var uri string
+	if len(os.Args) == 2 {
+		readPreference := os.Args[1]
+		uri = fmt.Sprintf("mongodb://sa:Password123@127.0.10.1:27017,127.0.10.2:27017,127.0.10.3:27017/?replicaSet=rs0&readPreference=%s", readPreference)
+	} else {
+		uri = "mongodb://sa:Password123@127.0.10.1:27017,127.0.10.2:27017,127.0.10.3:27017/?replicaSet=rs0"
+	}
+	fmt.Println(uri)
+
+	clientOpts := options.Client().ApplyURI(uri).SetServerMonitor(srvMonitor).SetMonitor(cmdMonitor)
+
+	client, err := mongo.Connect(context.TODO(), clientOpts)
 	if err != nil {
 		log.Println(err)
 	}
@@ -22,37 +96,51 @@ func main() {
 			log.Fatal(err)
 		}
 	}()
-	DB := client.Database("newDB")
+
+	DB := client.Database("newDB2")
 	users := DB.Collection("user")
-	queries.MongoWrite(users, ctx)
-	queries.MongoRead(users, ctx)
 
-	//============ Code for write data
-	//log.Println("Entering flexible data before schema altering")
-	//adding flexible data
-	// result, err := users.InsertMany(ctx, []interface{}{
-	// 	bson.D{{Key: "name", Value: "A"}, {Key: "email", Value: "Toni Morrison"}, {Key: "age", Value: 24}},
-	// 	bson.D{{Key: "name", Value: "B"}, {Key: "email", Value: "Toni Morrison"}, {Key: "age", Value: 47}},
-	// 	bson.D{{Key: "name", Value: "C"}, {Key: "phone", Value: "934252345234"}, {Key: "sex", Value: "F"}},
-	// 	bson.D{{Key: "name", Value: "D"}, {Key: "email", Value: "Toni Morrison"}, {Key: "age", Value: 24}},
-	// 	bson.D{{Key: "name", Value: "E"}, {Key: "email", Value: "Toni Morrison"}, {Key: "age", Value: 47}},
-	// 	bson.D{{Key: "name", Value: "F"}, {Key: "phone", Value: "934252345234"}, {Key: "sex", Value: "F"}},
-	// 	bson.D{{Key: "name", Value: "G"}, {Key: "email", Value: "Toni Morrison"}, {Key: "age", Value: 24}},
-	// 	bson.D{{Key: "name", Value: "h"}, {Key: "email", Value: "Toni Morrison"}, {Key: "age", Value: 47}},
-	// 	bson.D{{Key: "name", Value: "i"}, {Key: "phone", Value: "934252345234"}, {Key: "sex", Value: "F"}},
-	// 	bson.D{{Key: "name", Value: "j"}, {Key: "phone", Value: "934252345234"}, {Key: "sex", Value: "F"}},
-	// })
-	// log.Println("Inserted flexible data before schema altering")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	defer logEvents()
+	
+	for j := range lst{
+		startWrite := time.Now()
+		for i := 0; i < lst[j]; i++ {
+			queries.MongoWrite(users, ctx)
+		}
+		res1,res2, _:= getReplicaSetStatus(client, ctx)
+		
+		elapsedWrite := time.Since(startWrite)
+		log.Printf("MongoWrite took %s for %d iterations", elapsedWrite, lst[j])
+		startRead := time.Now()
+		queries.MongoRead(users, ctx)
+		elapsedRead := time.Since(startRead)
+		log.Printf("MongoRead took %s", elapsedRead)
 
-	// log.Println(result)
 
-	// //=========== Code for read data
-	// log.Println("All collections")
-	// userCursor, _ := users.Find(ctx, bson.D{})
-	// userCursor.All(ctx, &userData)
-	// //log.Println(userData)
-	// for _, user := range userData {
-	// 	fmt.Println(user)
-	// }
+		// Log the servers used for write and read operations
+		log.Println("Write operations were performed on the following servers:")
+		for server, count := range writeServers {
+			log.Printf("Server: %s, Count: %d", server, count)
+		}
 
+		log.Println("Read operations were performed on the following servers:")
+		for server, count := range readServers {
+			log.Printf("Server: %s, Count: %d", server, count)
+		}
+
+}
+	file, err := os.OpenFile("events.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %s", err)
+	}
+	defer file.Close()
+
+	// Create a logger that writes to the file
+	logger := log.New(file, "", log.LstdFlags)
+
+	for i := range eventArray {
+		logger.Println("Writing Events", eventArray[i])
+	}
 }
--- queries/mongo_test.go
+++ queries/mongo_test.go
@@ -6,14 +6,45 @@ import (
 	"testing"
 	"time"
 
+	"go.mongodb.org/mongo-driver/event"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var eventArray []*event.ServerDescriptionChangedEvent
+var insertCount int = 0
+
+// var srvMonitor *event.ServerMonitor = &event.ServerMonitor{
+// 	ServerDescriptionChanged: func(e *event.ServerDescriptionChangedEvent) {
+// 		eventArray = append(eventArray, e)
+// 	},
+// }
+
+var cmdMonitor *event.CommandMonitor = &event.CommandMonitor{
+	Started: func(_ context.Context, evt *event.CommandStartedEvent) {
+		// log.Printf("Started command: %s %s\n", evt.CommandName, evt.Command)
+	},
+	Succeeded: func(_ context.Context, evt *event.CommandSucceededEvent) {
+		if evt.CommandName == "insert"{
+			insertCount += 1
+		}
+		// log.Printf("Succeeded command: %s %v\n", evt.CommandName, evt.Reply)
+	},
+	Failed: func(_ context.Context, evt *event.CommandFailedEvent) {
+		// log.Printf("Failed command: %s %v\n", evt.CommandName, evt.Failure)
+	},
+}
+
+func logEvents() {
+	log.Println("Total Insertions :", insertCount)
+}
+
 func BenchmarkMongoWrite(b *testing.B) {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	clientOpts := options.Client().ApplyURI("mongodb://sa:Password123@127.0.10.1:27017,127.0.10.2:27017,127.0.10.3:27017/?replicaSet=rs0").SetMonitor(cmdMonitor)
+	client, err := mongo.Connect(context.TODO(), clientOpts)
+
 	if err != nil {
 		log.Println(err)
 	}
@@ -33,60 +64,67 @@ func BenchmarkMongoWrite(b *testing.B) {
 		//readDuration := time.Since(startTime)
 		//fmt.Printf("Write operation took %v\n", readDuration)
 	}
+	logEvents()
+
+
 }
 
-func BenchmarkMongoRead(b *testing.B) {
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		log.Println(err)
-	}
-	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
-			log.Fatal(err)
-		}
-	}()
-	MongoDatabase := client.Database("newDB2")
-	User := MongoDatabase.Collection("user")
-	User.Drop(ctx)
+// func BenchmarkMongoRead(b *testing.B) {
+// 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+// 	for i := range eventArray {
+// 		log.Println("Writing Events", eventArray[i])
+// 	}
+// 	defer cancel()
+// 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://sa:Password123@127.0.10.1:27017,127.0.10.2:27017,127.0.10.3:27017/?replicaSet=rs0"))
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	defer func() {
+// 		if err = client.Disconnect(context.TODO()); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}()
+// 	MongoDatabase := client.Database("newDB2")
+// 	User := MongoDatabase.Collection("user")
 
-	b.ResetTimer()
-	//startTime := time.Now()
-	for i := 0; i < b.N; i++ {
-		//startTime = time.Now()
-		MongoRead(User, ctx)
-		//readDuration := time.Since(startTime)
-		//fmt.Printf("Read operation took %v\n", readDuration)
+// 	b.ResetTimer()
+// 	//startTime := time.Now()
+// 	for i := 0; i < b.N; i++ {
+// 		//startTime = time.Now()
+// 		MongoRead(User, ctx)
+// 		//readDuration := time.Since(startTime)
+// 		//fmt.Printf("Read operation took %v\n", readDuration)
 
-	}
-}
+// 	}
 
-func BenchmarkMongoReadWriteParallel(b *testing.B) {
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		log.Println(err)
-	}
-	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
-			log.Fatal(err)
-		}
-	}()
-	MongoDatabase := client.Database("newDB2")
-	User := MongoDatabase.Collection("user")
+// }
 
-	b.ResetTimer()
-	b.RunParallel(func(pb *testing.PB) {
-		//startTime := time.Now()
-		for pb.Next() {
-			//startTime = time.Now()
-			MongoRead(User, ctx)
-			MongoWrite(User, ctx)
-			//readDuration := time.Since(startTime)
-			//	fmt.Printf("Read operation took %v\n", readDuration)
+// func BenchmarkMongoReadWriteParallel(b *testing.B) {
+// 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+// 	defer cancel()
+// 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://sa:Password123@127.0.10.1:27017,127.0.10.2:27017,127.0.10.3:27017/?replicaSet=rs0").SetServerMonitor(srvMonitor))
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	defer func() {
+// 		if err = client.Disconnect(context.TODO()); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}()
+// 	MongoDatabase := client.Database("newDB2")
+// 	User := MongoDatabase.Collection("user")
 
-		}
-	})
-}
+// 	b.ResetTimer()
+// 	b.RunParallel(func(pb *testing.PB) {
+// 		//startTime := time.Now()
+// 		for pb.Next() {
+// 			//startTime = time.Now()
+// 			MongoRead(User, ctx)
+// 			MongoWrite(User, ctx)
+// 			//readDuration := time.Since(startTime)
+// 			//	fmt.Printf("Read operation took %v\n", readDuration)
+
+// 		}
+// 	})
+
+// }
